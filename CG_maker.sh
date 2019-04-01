1f1639ef46db3d1dd1593e279a04149219acc6a1
xiaodong yang <xiaodongy86@gmail.com>
2019-04-01 14:18:48 -0400

    Create CG_maker.sh

---
+++ CG_maker.sh
@@ -0,0 +1,30 @@
+cat tomato_erne_CG_maker.sh
+# ls *erne_meth.txt|sed 's#\(tomato_erne_\)\(.*\)\(_erne_meth\)\(.txt\)#cat \1\2\3\4 |egrep "CG." > \1\2\3_CG\4#' > tomato_erne_CG_maker.sh
+cat tomato_erne_12-P1_erne_meth.txt |egrep "CG." > tomato_erne_12-P1_erne_meth_CG.txt
+cat tomato_erne_12-P2_erne_meth.txt |egrep "CG." > tomato_erne_12-P2_erne_meth_CG.txt
+cat tomato_erne_12-P3_erne_meth.txt |egrep "CG." > tomato_erne_12-P3_erne_meth_CG.txt
+cat tomato_erne_13-P1_erne_meth.txt |egrep "CG." > tomato_erne_13-P1_erne_meth_CG.txt
+cat tomato_erne_13-P2_erne_meth.txt |egrep "CG." > tomato_erne_13-P2_erne_meth_CG.txt
+cat tomato_erne_13-P3_erne_meth.txt |egrep "CG." > tomato_erne_13-P3_erne_meth_CG.txt
+cat tomato_erne_1-P1_erne_meth.txt |egrep "CG." > tomato_erne_1-P1_erne_meth_CG.txt
+cat tomato_erne_1-P2_erne_meth.txt |egrep "CG." > tomato_erne_1-P2_erne_meth_CG.txt
+cat tomato_erne_21-P17_erne_meth.txt |egrep "CG." > tomato_erne_21-P17_erne_meth_CG.txt
+cat tomato_erne_21-P19_erne_meth.txt |egrep "CG." > tomato_erne_21-P19_erne_meth_CG.txt
+cat tomato_erne_21-P1_erne_meth.txt |egrep "CG." > tomato_erne_21-P1_erne_meth_CG.txt
+cat tomato_erne_21-P22_erne_meth.txt |egrep "CG." > tomato_erne_21-P22_erne_meth_CG.txt
+cat tomato_erne_21-P32_erne_meth.txt |egrep "CG." > tomato_erne_21-P32_erne_meth_CG.txt
+cat tomato_erne_21-P34_erne_meth.txt |egrep "CG." > tomato_erne_21-P34_erne_meth_CG.txt
+cat tomato_erne_21-P37_erne_meth.txt |egrep "CG." > tomato_erne_21-P37_erne_meth_CG.txt
+cat tomato_erne_21-P3_erne_meth.txt |egrep "CG." > tomato_erne_21-P3_erne_meth_CG.txt
+cat tomato_erne_21-P6_erne_meth.txt |egrep "CG." > tomato_erne_21-P6_erne_meth_CG.txt
+cat tomato_erne_2-P1_erne_meth.txt |egrep "CG." > tomato_erne_2-P1_erne_meth_CG.txt
+cat tomato_erne_2-P2_erne_meth.txt |egrep "CG." > tomato_erne_2-P2_erne_meth_CG.txt
+cat tomato_erne_BE-P1_erne_meth.txt |egrep "CG." > tomato_erne_BE-P1_erne_meth_CG.txt
+cat tomato_erne_BE-P2_erne_meth.txt |egrep "CG." > tomato_erne_BE-P2_erne_meth_CG.txt
+cat tomato_erne_BE-P3_erne_meth.txt |egrep "CG." > tomato_erne_BE-P3_erne_meth_CG.txt
+cat tomato_erne_GE-P1_erne_meth.txt |egrep "CG." > tomato_erne_GE-P1_erne_meth_CG.txt
+cat tomato_erne_GE-P2_erne_meth.txt |egrep "CG." > tomato_erne_GE-P2_erne_meth_CG.txt
+cat tomato_erne_GE-P3_erne_meth.txt |egrep "CG." > tomato_erne_GE-P3_erne_meth_CG.txt
+
+
+
