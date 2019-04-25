02a0ec0c94230987c98ac43c176eab06dcc9f4dc
xiaodong yang <xiaodongy86@gmail.com>
2019-04-25 16:18:04 -0400

    Create trim_glore.sh

---
+++ trim_glore.sh
@@ -0,0 +1,38 @@
+```
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/12-P1 /data5/F15FTSUSAT0747_TOMrcwM/synology2/12-P1/12-P1_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/12-P1/12-P1_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/12-P2 /data5/F15FTSUSAT0747_TOMrcwM/synology2/12-P2/12-P2_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/12-P2/12-P2_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/12-P3 /data5/F15FTSUSAT0747_TOMrcwM/synology2/12-P3/12-P3_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/12-P3/12-P3_2.fq.gz
+
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/13-P1 /data5/F15FTSUSAT0747_TOMrcwM/synology2/13-P1/13-P1_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/13-P1/13-P1_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/13-P2 /data5/F15FTSUSAT0747_TOMrcwM/synology2/13-P2/13-P2_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/13-P2/13-P2_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/13-P3 /data5/F15FTSUSAT0747_TOMrcwM/synology2/13-P3/13-P3_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/13-P3/13-P3_2.fq.gz
+
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/1-P1 /data5/F15FTSUSAT0747_TOMrcwM/synology2/1-P1/1-P1_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/1-P1/1-P1_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/1-P2 /data5/F15FTSUSAT0747_TOMrcwM/synology2/1-P2/1-P2_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/1-P2/1-P2_2.fq.gz
+
+
+
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P1 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P1/21-P1_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P1/21-P1_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P17 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P17/21-P17_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P17/21-P17_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P19 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P19/21-P19_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P19/21-P19_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P22 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P22/21-P22_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P22/21-P22_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P3 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P3/21-P3_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P3/21-P3_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P32 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P32/21-P32_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P32/21-P32_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P34 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P34/21-P34_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P34/21-P34_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P37 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P37/21-P37_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P37/21-P37_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/21-P6 /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P6/21-P6_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/21-P6/21-P6_2.fq.gz
+
+
+
+
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/2-P1 /data5/F15FTSUSAT0747_TOMrcwM/synology2/2-P1/2-P1_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/2-P1/2-P1_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/2-P2 /data5/F15FTSUSAT0747_TOMrcwM/synology2/2-P2/2-P2_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/2-P2/2-P2_2.fq.gz
+
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/BE-P1 /data5/F15FTSUSAT0747_TOMrcwM/synology2/BE-P1/BE-P1_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/BE-P1/BE-P1_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/BE-P2 /data5/F15FTSUSAT0747_TOMrcwM/synology2/BE-P2/BE-P2_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/BE-P2/BE-P2_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/BE-P3 /data5/F15FTSUSAT0747_TOMrcwM/synology2/BE-P3/BE-P3_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/BE-P3/BE-P3_2.fq.gz
+
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/GE-P1 /data5/F15FTSUSAT0747_TOMrcwM/synology2/GE-P1/GE-P1_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/GE-P1/GE-P1_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/GE-P2 /data5/F15FTSUSAT0747_TOMrcwM/synology2/GE-P2/GE-P2_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/GE-P2/GE-P2_2.fq.gz
+/usr/local/bin05169/trim_galore --paired --trim1 --gzip --phred33 --fastqc --fastqc_args '-t 16' -o /data5/F15FTSUSAT0747_TOMrcwM/YXD_Bismark_pipeline/trim_glore/GE-P3 /data5/F15FTSUSAT0747_TOMrcwM/synology2/GE-P3/GE-P3_1.fq.gz /data5/F15FTSUSAT0747_TOMrcwM/synology2/GE-P3/GE-P3_2.fq.gz
+```
